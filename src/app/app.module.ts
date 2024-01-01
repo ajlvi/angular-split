fa995d3866a0963e9ff7872788eca9473bfad628
ajlvi <mtgeloproject@gmail.com>
2024-01-01 14:58:40 -0500

    input form connected

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { InputContainerComponent } from './input-container/input-container.component';
@@ -16,7 +17,7 @@ import { TimePipe } from './time.pipe';
     SplitRowComponent,
     TimePipe
   ],
-  imports: [BrowserModule, HttpClientModule],
+  imports: [BrowserModule, HttpClientModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent],
 })
--- src/app/input-container/input-form/input-form.component.ts
+++ src/app/input-container/input-form/input-form.component.ts
@@ -1,10 +1,15 @@
 import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-input-form',
   templateUrl: './input-form.component.html',
-  styleUrls: ['./input-form.component.css']
+  styleUrls: ['./input-form.component.css'],
 })
 export class InputFormComponent {
-
+  onSubmit(form: NgForm) {
+    const minutes = form.value.minutes;
+    const seconds = form.value.seconds;
+    console.log(minutes, seconds);
+  }
 }
